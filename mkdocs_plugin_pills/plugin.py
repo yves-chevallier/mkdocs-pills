750a7bd55f5994d8f25f5f38a7f8f6f5964bdda3
Yves Chevallier <yves.chevallier@heig-vd.ch>
2025-01-22 09:53:40 +0100

    language and config

--- mkdocs_plugin_pills/plugin.py
+++ mkdocs_plugin_pills/plugin.py
@@ -2,12 +2,17 @@
 
 from mkdocs.plugins import BasePlugin
 from mkdocs.utils import copy_file
+from mkdocs.config import base, config_options as c
 from .regex import RegexExtension
 from .unicode import UnicodeExtension
 from pathlib import Path
 
+class PillsConfig(base.Config):
+    language = c.Optional(c.Type(str))
+    regex = c.Type(bool, default=True)
+    unicode = c.Type(bool, default=True)
 
-class PillsPlugin(BasePlugin):
+class PillsPlugin(BasePlugin[PillsConfig]):
     """Plugin for mkdocs that adds custom markdown extensions."""
 
     def __init__(self):
@@ -17,8 +22,19 @@ class PillsPlugin(BasePlugin):
 
     def on_config(self, config):
         """Add the custom markdown extensions to the config."""
-        config["markdown_extensions"].append(RegexExtension())
-        config["markdown_extensions"].append(UnicodeExtension())
+
+        # Guess language if not provided
+        if not hasattr(self, 'language'):
+            if hasattr(config.theme, 'language'):
+                self.language = config.theme.language
+            elif hasattr(config.theme, 'locale'):
+                self.language = config.theme.locale.language
+
+        if self.config.regex:
+            config["markdown_extensions"].append(RegexExtension())
+
+        if self.config.unicode:
+            config["markdown_extensions"].append(UnicodeExtension(language=self.language))
 
         css = Path("css/pills.css")
         self.css_output_path = Path(config["site_dir"]) / css
--- mkdocs_plugin_pills/unicode.py
+++ mkdocs_plugin_pills/unicode.py
@@ -9,22 +9,31 @@ from xml.etree import ElementTree as etree
 from markdown.extensions import Extension
 from markdown.inlinepatterns import InlineProcessor
 
-BASE_URL = "https://symbl.cc/fr/"
-
+BASE_URL = "https://symbl.cc"
 
 class UnicodeProcessor(InlineProcessor):
+    def __init__(self, pattern, md, language="en"):
+        super().__init__(pattern, md)
+        self.language = language
+
     def handleMatch(self, m, data):
         unicode_code = m.group(1)
         el = etree.Element("a")
-        el.set("href", f"{BASE_URL}/{unicode_code}")
+        el.set("href", f"{BASE_URL}/{self.language}/{unicode_code}")
         el.set("class", "ycr-pill ycr-unicode")
         el.set("target", "_blank")
         el.text = f"{unicode_code}"
         return el, m.start(0), m.end(0)
 
+    def set_language(self, language):
+        self.language = language
+
 
 class UnicodeExtension(Extension):
+    def __init__(self, language='en'):
+        self.language = language
+
     def extendMarkdown(self, md):
         UNICODE_RE = r"\bU\+([0-9A-F]{4,5})\b"
-        unicode_pattern = UnicodeProcessor(UNICODE_RE, md)
+        unicode_pattern = UnicodeProcessor(UNICODE_RE, md, self.language)
         md.inlinePatterns.register(unicode_pattern, "unicode", 175)
